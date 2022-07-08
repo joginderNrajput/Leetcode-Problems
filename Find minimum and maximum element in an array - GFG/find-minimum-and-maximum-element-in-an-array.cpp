ece1c380bc87300c878bb65840eaf0887dec7f46
Joginder Singh <85634213+joginderNrajput@users.noreply.github.com>
2022-07-08 20:01:43 +0530

    Added solution - LeetHub

---
+++ Find minimum and maximum element in an array - GFG/find-minimum-and-maximum-element-in-an-array.cpp
@@ -0,0 +1,34 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+pair<long long, long long> getMinMax(long long a[], int n) ;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        ll a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        pair<ll, ll> pp = getMinMax(a, n);
+
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+pair<long long, long long> getMinMax(long long a[], int n) {
+    vector<int> temp;
+    pair<int, int> ans;
+    for(int i=0; i<n; i++){
+        temp.push_back(a[i]);
+    }
+    sort(temp.begin(), temp.end());
+    
+    return {temp[0], temp[n-1]};
+}
