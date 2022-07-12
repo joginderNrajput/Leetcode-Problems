9ba0fd621519769346440776395f2d059468abeb
Joginder Singh <85634213+joginderNrajput@users.noreply.github.com>
2022-07-12 08:45:01 +0530

    Added solution - LeetHub

--- Cyclically rotate an array by one - GFG/cyclically-rotate-an-array-by-one.c
+++ Cyclically rotate an array by one - GFG/cyclically-rotate-an-array-by-one.c
@@ -27,10 +27,8 @@ int main()
 void rotate(int arr[], int n)
 {
     int temp = arr[n-1];
-    for(int i=n-1; i>0; i--){
-        
-        arr[i] = arr[i-1];
-       
-    }
+   for(int i=n-1; i>0; i--){
+       arr[i] = arr[i-1];
+   }
      arr[0] = temp;
 }
