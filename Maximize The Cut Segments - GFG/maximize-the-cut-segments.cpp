ad5f127dbb6ae9e7b57626625a32a91fa59a9788
Joginder Singh <85634213+joginderNrajput@users.noreply.github.com>
2022-10-12 17:13:59 +0530

    Added solution - LeetHub

--- Maximize The Cut Segments - GFG/maximize-the-cut-segments.cpp
+++ Maximize The Cut Segments - GFG/maximize-the-cut-segments.cpp
@@ -10,28 +10,29 @@ class Solution
 {
     public:
     //Function to find the maximum number of cuts.
-    int solve(int n, int x, int y, int z, vector<int> &dp){
-        if(n == 0){
-            return 0;
-        }
-        if(n < 0){
-            return INT_MIN;
-        }
-        if(dp[n] != -1){
-            return dp[n];
-        }
+    int solve(int n, int x, int y, int z){
+        vector<int>dp(n+1, INT_MIN);
+        dp[0] = 0;
         
-        
-        int a = solve(n-x, x, y, z, dp)+1;
-        int b = solve(n-y, x, y, z, dp)+1;
-        int c = solve(n-z, x, y, z, dp)+1;
-        dp[n] = max(a, max(b, c));
+        for(int i=1; i<=n; i++){
+            if(i-x >= 0){
+                dp[i] = max(dp[i], dp[i-x]+1);
+            }
+            
+            if(i-y >= 0){
+                dp[i] = max(dp[i], dp[i-y]+1);
+            }
+            if(i-z >= 0){
+                dp[i] = max(dp[i], dp[i-z]+1);
+            }
+            
+        }
         return dp[n];
     }
     int maximizeTheCuts(int n, int x, int y, int z)
     {
         vector<int>dp(n+1, -1);
-        int ans = solve(n, x, y, z, dp);
+        int ans = solve(n, x, y, z);
        if(ans < 0){
            return 0;
        }else{
