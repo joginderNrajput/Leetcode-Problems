c7d159aff03b5f82b8eae05fe4067d431de6b115
Joginder Singh <85634213+joginderNrajput@users.noreply.github.com>
2022-10-16 13:18:00 +0530

    Added solution - LeetHub

--- Get Minimum Squares - GFG/get-minimum-squares.cpp
+++ Get Minimum Squares - GFG/get-minimum-squares.cpp
@@ -9,24 +9,23 @@ using namespace std;
 
 class Solution{
 	public:
-	int solve(int n, vector<int> &dp){
-	    if(n == 0){
-	        return 0;
-	    }
-	    if(dp[n] != -1){
-	        return dp[n];
-	    }
-	    int ans = n;
-	    for(int i=1; i*i <= n; i++){
-	        ans = min(ans,1+ solve(n - (i*i), dp));
-	    }
-	    dp[n] = ans;
+	int solve(int n){
+	    vector<int>dp(n+1, INT_MAX);
+	    dp[0] = 0;
+	    
+	   for(int i=1; i<=n; i++){
+	       for(int j=1; j*j <= n; j++){
+	           if(i-(j*j) >= 0){
+	           dp[i] = min(dp[i], 1+dp[i - (j*j)]);
+	           }
+	       }
+	   }
 	    return dp[n];
 	}
 	int MinSquares(int n)
 	{
 	    vector<int>dp(n+1, -1);
-	    return solve(n, dp);
+	    return solve(n);
 	}
 };
 
