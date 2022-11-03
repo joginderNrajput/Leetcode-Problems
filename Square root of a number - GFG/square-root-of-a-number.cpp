2449748f1ae4918544aced899dbd6b2bfbc00048
Joginder Singh <85634213+joginderNrajput@users.noreply.github.com>
2022-11-03 20:25:03 +0530

    Added solution - LeetHub

--- Square root of a number - GFG/square-root-of-a-number.cpp
+++ Square root of a number - GFG/square-root-of-a-number.cpp
@@ -9,10 +9,38 @@ using namespace std;
 // x: element to find square root
 class Solution{
   public:
+      long long int solve(long long int n){
+          long long int s = 0;
+          long long int e = n-1;
+          long long mid = (s+e)/2;
+          long long ans = 0;
+          if(n == 1){
+              return 1;
+          }
+          
+          while(s <= e){
+            long long square = mid*mid;
+              if(square == n){
+                  return mid;
+              }
+              
+              if(square < n){
+                  ans = mid;
+                  s = mid+1;
+              }
+              
+              if(square > n){
+                  e = mid-1;
+              }
+              mid = (s+e)/2;
+          }
+          return mid;
+      }
     long long int floorSqrt(long long int x) 
     {
         long long int ans = sqrt(x);
         return ans;
+        // return solve(x);
     }
 };
 
